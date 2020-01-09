14858abfbd1dcef181e11e25e7216a90756b02e1
Vladimir Grubor <grubor@gmail.com>
2020-01-09 23:49:34 +0100

    Updates

---
+++ precache-manifest.52e067a5d04afb9051579b3832b508a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55b0906b5e1d30a96fb0f3ba3ea8c2dd",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "32d221ad238f4e14c509",
+    "url": "/memory-game/static/js/2.43dcdb27.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/memory-game/static/js/2.43dcdb27.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9ba57a12347e39a20d4c",
+    "url": "/memory-game/static/js/main.b38eb2f3.chunk.js"
+  },
+  {
+    "revision": "c3e29c6f82ed5623f2d2",
+    "url": "/memory-game/static/js/runtime-main.ac160a44.js"
+  }
+]);
